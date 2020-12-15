c691c5fc6c70b5ff41cdd0f451b1fa653bcf77a4
ararita <munjizavesna@gmail.com>
2020-12-15 16:44:09 +0100

    Last changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-  return (
-    <div>
-      Made In Berlin etc.
-    </div>
-  )
+  return <div>Made In Berlin etc</div>;
 }
