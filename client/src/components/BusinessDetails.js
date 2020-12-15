4e72d2bd8dedbe6b5c9d55b7b7c90cb26eea48f1
Leonie Scholl <liecrazed@gmail.com>
2020-12-15 12:03:00 +0100

    Busines Details

--- client/src/components/BusinessDetails.js
+++ client/src/components/BusinessDetails.js
@@ -11,7 +11,7 @@ export default class BusinessDetails extends Component {
     headOfBusiness: "",
     picture: "",
     description: "",
-    category: [],
+    category: "",
     street: "",
     houseNumber: "",
     city: "",
