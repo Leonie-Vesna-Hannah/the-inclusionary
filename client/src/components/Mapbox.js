de52cc7c3106d7ef05ecbe7e5cd2357082dc9a56
ararita <munjizavesna@gmail.com>
2020-12-16 14:11:28 +0100

    Add map functionality

--- client/src/components/Mapbox.js
+++ client/src/components/Mapbox.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
 import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken =
@@ -6,9 +8,13 @@ mapboxgl.accessToken =
 
 class Mapbox extends Component {
   state = {
+    // viewport: {
+    // width: "30vh",
+    // height: "30vh",
     lng: this.props.long,
     lat: this.props.lat,
-    zoom: 14,
+    zoom: 12,
+    // },
   };
 
   componentDidMount() {
@@ -36,19 +42,40 @@ class Mapbox extends Component {
       ],
     };
 
-    geojson.features.forEach(function (marker) {
+    geojson.features.forEach((marker) => {
       // create a HTML element for each feature
       var el = document.createElement("div");
       el.className = "marker";
 
       // make a marker for each feature and add to the map
-      new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }))
+        .addTo(map);
     });
   }
   render() {
+    console.log("state from mapbox", this.state);
     return (
       <div>
+        {/* <Marker
+          coordinates={[this.state.lng, this.state.lat]}
+          anchor="bottom"
+        /> */}
         <div ref={(el) => (this.mapContainer = el)} />
+        {/* <ReactMapGL
+          {...this.state.viewport}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          onViewportChange={(viewport) => this.setState({ viewport })}
+          mapboxApiAccessToken="pk.eyJ1IjoidmVzbmFtIiwiYSI6ImNraXEzMXk2NzBjczgyc3A5NzM1cnZ4eHUifQ.3lDAsenrxdP6Sq4WVSo43g"
+        >
+          <Marker
+            latitude={this.state.viewport.lat}
+            longitude={this.state.viewport.lng}
+          >
+            I'm Here!!!
+          </Marker>
+        </ReactMapGL> */}
       </div>
     );
   }
